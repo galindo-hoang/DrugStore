ba281ce76a01a04665d7fb922956e9e7d7106868
huy <hmhuy191101@gmail.com>
2022-03-18 13:22:11 +0700

    fragment bottom navigation

--- app/src/main/java/com/example/drugstore/activities/HomeActivity.kt
+++ app/src/main/java/com/example/drugstore/activities/HomeActivity.kt
@@ -1,14 +1,43 @@
 package com.example.drugstore.activities
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
-import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.drugstore.R
-import com.example.drugstore.firebase.FirebaseClass
+import com.example.drugstore.databinding.ActivityHomeBinding
+import com.example.drugstore.fragment.*
+import com.google.android.material.navigation.NavigationBarView
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+        binding.btnNavView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navBtnHome -> {
+                    replaceFragment(HomeFragment())
+                }
+                R.id.navBtnOrder -> {
+                    replaceFragment(OrderFragment())
+                }
+                R.id.navBtnNotify -> {
+                    replaceFragment(NotificationFragment())
+                }
+                else -> replaceFragment(ProfileFragment())
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentBottomNav.id,fragment)
+        fragmentTransaction.commit()
     }
 }
