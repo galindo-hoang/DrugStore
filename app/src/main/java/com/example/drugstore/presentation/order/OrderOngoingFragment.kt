678a583afb93c6288ed1f468be04354ac405bd66
huy <hmhuy191101@gmail.com>
2022-04-15 21:04:22 +0700

    done order and view order history

--- app/src/main/java/com/example/drugstore/presentation/order/OrderOngoingFragment.kt
+++ app/src/main/java/com/example/drugstore/presentation/order/OrderOngoingFragment.kt
@@ -1,12 +1,21 @@
 package com.example.drugstore.presentation.order
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.drugstore.R
-import com.example.drugstore.databinding.FragmentOrderOngoingBinding
+import com.example.drugstore.data.firebase.FirebaseClass
+import com.example.drugstore.databinding.FragmentOrderStatusBinding
+import com.example.drugstore.presentation.adapter.OrderAdapter
+import com.example.drugstore.presentation.adapter.OrderProductAdapter
+import com.example.drugstore.presentation.home.ProductVM
+import com.example.drugstore.utils.Constants
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,11 +27,13 @@ private const val ARG_PARAM2 = "param2"
  * Use the [OrderFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class OrderOngoingFragment : Fragment() {
+class OrderStatusFragment : Fragment() {
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var binding: FragmentOrderOngoingBinding
+    private lateinit var binding: FragmentOrderStatusBinding
+    private val orderVM: OrderVM by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,8 +47,19 @@ class OrderOngoingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_order_ongoing, container, false)
+        binding = FragmentOrderStatusBinding.inflate(inflater,container,false)
 
+
+
+
+
+        val orderAdapter = OrderAdapter()
+        orderVM.getOrderByUser(FirebaseClass.getCurrentUserId(),false).observe(viewLifecycleOwner){
+            it?.let { it1 -> orderAdapter.setList(it1) }
+        }
+        binding.rcViewOrderOngoing.adapter = orderAdapter
+        binding.rcViewOrderOngoing.layoutManager = LinearLayoutManager(context)
+        return binding.root
     }
 
     companion object {
@@ -52,7 +74,7 @@ class OrderOngoingFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            OrderOngoingFragment().apply {
+            OrderStatusFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
---
+++ app/src/main/java/com/example/drugstore/presentation/order/OrderStatusActivity.kt
@@ -0,0 +1,12 @@
+package com.example.drugstore.presentation.order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.drugstore.R
+
+class OrderStatusActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_status)
+    }
+}
